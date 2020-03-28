460aa3d54c03531c9a752e6e3e146363a87c98bc
Simran <simran.maurya_cs18@gla.ac.in>
2020-03-28 13:33:02 +0530

    generate getters and setters for int variable count

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -61,5 +61,11 @@ public class MyBinarySearchTree {
             inOrder(current.getRight());
         }
     }
+    public int getCount(){
+        return count;
+    }
+    public void setCount(int count){
+        this.count=count;
+    }
 }
 
