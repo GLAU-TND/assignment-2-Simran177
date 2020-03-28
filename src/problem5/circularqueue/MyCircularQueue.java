7292ba3460474dae526e31409d79e2a88e4a6d51
Simran <simran.maurya_cs18@gla.ac.in>
2020-03-28 16:30:53 +0530

    Getters and setters

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -13,7 +13,7 @@ public class MyCircularQueue {
     private int last;
     private int maxSize;
     private Student[] circularQueue;
-    
+
     public MyCircularQueue(int maxSize){
         this.maxSize=maxSize;
         circularQueue =new Student[maxSize];
@@ -22,4 +22,43 @@ public class MyCircularQueue {
         this.size=0;
     }
 
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getFirst() {
+        return first;
+    }
+
+    public void setFirst(int first) {
+        this.first = first;
+    }
+
+    public int getLast() {
+        return last;
+    }
+
+    public void setLast(int last) {
+        this.last = last;
+    }
+
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    public void setMaxSize(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public Student[] getCircularQueue() {
+        return circularQueue;
+    }
+
+    public void setCircularQueue(Student[] circularQueue) {
+        this.circularQueue = circularQueue;
+    }
 }
