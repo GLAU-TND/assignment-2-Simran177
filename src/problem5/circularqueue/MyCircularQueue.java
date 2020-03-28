0e881e082a9bbc0a2dc9213187724dafc5fac851
Simran <simran.maurya_cs18@gla.ac.in>
2020-03-28 16:26:03 +0530

    Declaring private variables size,first,last to hold the size of circular queue,hold the first and last node respectively. Parameterized Constructor to assign value to each.

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -6,5 +6,20 @@
  */
 package problem5.circularqueue;
 //to implement circular queue
+import problem5.student.Student;
 public class MyCircularQueue {
+    private int size;
+    private int first;
+    private int last;
+    private int maxSize;
+    private Student[] circularQueue;
+    
+    public MyCircularQueue(int maxSize){
+        this.maxSize=maxSize;
+        circularQueue =new Student[maxSize];
+        this.first=0;
+        this.last=0;
+        this.size=0;
+    }
+
 }
